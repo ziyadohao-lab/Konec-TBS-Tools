603883c2658041210784ed7b85f77598728e4b25
ziyadohao-lab <ziyadohao@gmail.com>
2026-03-11 10:23:13 +1100

    Add files via upload

---
+++ Konec TBS.py
@@ -0,0 +1,173 @@
+import streamlit as st
+
+st.set_page_config(page_title="Trouble shooting", page_icon="🔧")
+
+st.title("Trouble shooting Tool")
+
+if "step" not in st.session_state:
+    st.session_state.step = 0
+if "code" not in st.session_state:
+    st.session_state.code = None
+
+
+if st.session_state.code is not None:
+
+    code = st.session_state.code
+
+    st.success(f"Diagnosis Code: {code}")
+
+    if code == 1:
+        st.write("AP / Internet issue")
+
+    elif code == 2:
+        st.write("Check Automation")
+
+    elif code == 3:
+        st.write("IP Issue")
+
+    elif code == 4:
+        st.write("IP Issue")
+
+    elif code == 5:
+        st.write("Internet Issue")
+
+    elif code == 6:
+        st.write("Cooktop damage not switch related issue")
+
+    elif code == 7:
+        st.write("Switch damage please contact professional for repair or replacement")
+
+    elif code == 8:
+        st.write("Please open the breaker")
+
+    elif code == 9999999:
+        st.write("Done!")
+    
+    else:
+        st.write("Pending, waiting for update")
+
+    if st.button("Restart"):
+        st.session_state.step = 0
+        st.session_state.code = None
+        st.rerun()
+
+    st.stop()
+
+def next_step(step):
+    st.session_state.step = step
+    st.rerun()
+
+# Select Product
+if st.session_state.step == 0:
+
+    st.subheader("Select the problematic device")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    if col1.button("Switch"):
+        next_step(1)
+
+    if col2.button("Lock"):
+        next_step(501)
+
+    if col3.button("HPS"):
+        next_step(1001)
+
+    if col4.button("GPO"):
+        next_step(1501)
+
+    if col5.button("AC Gateway"):
+        next_step(2001)
+
+
+# Switch 
+# Step 1 Which Switch
+if st.session_state.step == 1:
+
+    st.subheader("Which switch is experiencing a problem?")
+
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    if col1.button("Entry Switch"):
+        next_step(2)
+
+    if col2.button("Bathroom Switch"):
+        next_step(501)
+
+    if col3.button("Kitchen Switch"):
+        next_step(1001)
+
+    if col4.button("Cooktop Switch"):
+        next_step(1501)
+
+    if col5.button("Desk Switch"):
+        next_step(2001)
+
+    if col6.button("Back"):
+        next_step(0)
+
+
+# Step 2 Entry Switch Common Issue
+elif st.session_state.step == 2:
+
+    st.subheader("Common Issue")
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    if col1.button("Issue1"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col2.button("Issue2"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col3.button("Issue3"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col4.button("Back"):
+        next_step(1)
+
+    
+
+
+# Lock
+elif st.session_state.step == 501:
+
+    st.subheader("Common Issue")
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    if col1.button("Issue1"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col2.button("Issue2"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col3.button("Issue3"):
+        st.session_state.code = 8000000
+        st.rerun()
+
+    if col4.button("Back"):
+        next_step(1)
+
+
+# HPS
+elif st.session_state.step == 1001:
+        st.session_state.code = 8000000
+        st.rerun()
+
+
+# GPO
+elif st.session_state.step == 1501:
+        st.session_state.code = 8000000
+        st.rerun()
+
+
+# AC Gateway
+elif st.session_state.step == 2001:
+        st.session_state.code = 8000000
+        st.rerun()
