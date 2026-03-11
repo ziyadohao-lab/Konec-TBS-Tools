9c03eaa1b9e5fe80631bf0f01ea16dd7465f7ae6
ziyadohao-lab <ziyadohao@gmail.com>
2026-03-12 10:16:31 +1100

    Update Konec TBS.py

--- Konec TBS.py
+++ Konec TBS.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.image("logo.png", width=400)
+st.image("logo.png")
 st.set_page_config(page_title="Trouble shooting", page_icon="🔧", layout="wide")
 
 st.title("Trouble shooting Tool")
@@ -355,6 +355,7 @@ st.html("""
         
     </style>
 """)
+
 
 
 
